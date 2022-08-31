81bc8ccbb7b518c8090df2e5b69699f015d21b21
hangeol.park <hangeol.park@lge.com>
2022-08-31 16:28:35 +0900

    initial commit

---
+++ js/system_time.js
@@ -0,0 +1,22 @@
+// ref: https://webostv.developer.lge.com/api/webos-service-api/system-service/
+function getWebOSSystemTime(handleFunc, subscribe) {
+  return webOS.service.request("luna://com.palm.systemservice", {
+    method: "time/getSystemTime",
+    parameters: { subscribe: subscribe },
+    onSuccess: function (inResponse) {
+      if (!inResponse.subscribed) {
+        console.log("Failed to subscribe the system time information");
+        return;
+      }
+
+      console.log("Result: " + JSON.stringify(inResponse));
+      handleFunc(inResponse);
+    },
+    onFailure: function (inError) {
+      console.log("Failed to get system time information");
+      console.log("[" + inError.errorCode + "]: " + inError.errorText);
+      // To-Do something
+      return;
+    },
+  });
+}
